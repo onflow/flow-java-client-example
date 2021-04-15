cbdc287377dfe798d4bd7816a260036a94a12573
Jeremy Arnold <jeremy@possiblyfaulty.com>
2021-04-15 15:29:14 -0600

    Java test now works correctly on emulator settings

--- src/test/java/org/onflow/examples/java/AppTest.java
+++ src/test/java/org/onflow/examples/java/AppTest.java
@@ -9,9 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
-    public static final String SERVICE_PRIVATE_KEY_HEX = "b2bee95fcf0afab690c2f61d8787de0dc9cb2a64eee797d11f6ae8bc29d5f92b";
-    public static final String USER_PRIVATE_KEY_HEX = "16285d55d0f6eb0de3978afd4f406f3796cc750dcd6e5877ba0da9837acf8dc8c30cc098334b8e0a5b5ee0ff6c5e5813178b88a9035f97417e869e9fd58dd7eb";
-    public static final String USER_PUBLIC_KEY_HEX = "199e54b7031b6d96f881b94ef3582deca90f7a2c7d180899193a22712be264bdbce5de5eac3e8afaedd75c48f620395f093af75eee20b617a7bdbae08e4fcb41";
+    public static final String SERVICE_PRIVATE_KEY_HEX = "a2f983853e61b3e27d94b7bf3d7094dd756aead2a813dd5cf738e1da56fa9c17";
+    public static final String USER_PUBLIC_KEY_HEX = "09c883f8c036bcf2607607379fd332374bdd2420990810d89b7af4fdf019e3e0423b07a0103388e62ac4775586f268ce3cb90acd688a6ccaab6d4035303e6acf";
 
     private FlowAddress serviceAccountAddress = new FlowAddress("f8d6e0586b0a20c7");
 
